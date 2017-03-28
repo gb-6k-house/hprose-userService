308f57b8b690e1eb4cb51d375f2f594023843fa0
oschina6k <csu_liukai_web@163.com>
2017-03-28 16:12:21 +0800

    feat:(common) 构建项目基本结构

---
+++ configes/confige.js
@@ -0,0 +1,10 @@
+/**
+ * Created by niupark on 16/1/6.
+ */
+exports.debug = true;
+exports.port = 3000;
+exports.charPort = 3001;
+exports.session_secret = 'scott todo session secret';
+exports.root=__dirname;
+exports.host={'port':6002, 'ip':'127.0.0.1'};
+
